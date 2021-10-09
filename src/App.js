1d605e897e42db85bb12f0c1c0a04f1762b182fc
tauhid102 <tauhidurr102@gmail.com>
2021-10-09 18:46:48 +0600

    set facebook authentication

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import './App.css';
-import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, signOut } from "firebase/auth";
+import { getAuth, signInWithPopup, GoogleAuthProvider, GithubAuthProvider, signOut, FacebookAuthProvider } from "firebase/auth";
 import initializeAuthentication from './Firebase/Firebaseinitialize';
 import { useState } from 'react';
 
 const provider = new GoogleAuthProvider();
 const gitHubProvider = new GithubAuthProvider();
+const facebookProvider = new FacebookAuthProvider();
 
 initializeAuthentication();
 
@@ -38,24 +39,55 @@ function App() {
         setUser(loggedInUser);
       });
   }
-  const handleSignout=()=>{
+  const handleSignout = () => {
     signOut(auth).then(() => {
       setUser({});
     })
   }
+  const facebookSignIn = () => {
+    signInWithPopup(auth, facebookProvider)
+      .then((result) => {
+        // The signed-in user info.
+        const user = result.user;
+        console.log(result);
+        // This gives you a Facebook Access Token. You can use it to access the Facebook API.
+        const credential = FacebookAuthProvider.credentialFromResult(result);
+        const accessToken = credential.accessToken;
+
+        // ...
+      })
+      .catch((error) => {
+        // Handle Errors here.
+        const errorCode = error.code;
+        const errorMessage = error.message;
+        // The email of the user's account used.
+        const email = error.email;
+        // The AuthCredential type that was used.
+        const credential = FacebookAuthProvider.credentialFromError(error);
+
+        // ...
+      });
+  }
   return (
     <div className="App">
-      <button onClick={handleGoogleSignIN}>Google Sign In</button>
-      <button onClick={handleGithubSignIN}>Github Sign In</button>
-      <button onClick={handleSignout}>Sign Out</button>
-      {
-        user.email && <div>
-           <h2>Nmae: {user.name}</h2>
-           <h4>Email: {user.email}</h4>
-           <img src={user.photoURL} alt="" />
-           </div>
+      {!user.name ?
+        <div>
+          <button onClick={handleGoogleSignIN}>Google Sign In</button>
+          <button onClick={handleGithubSignIN}>Github Sign In</button>
+          <button onClick={facebookSignIn}>Facebook Sign in</button>
+        </div>
+        :
+        <button onClick={handleSignout}>Sign Out</button>
       }
-      
+
+      {
+        user.name && <div>
+          <h2>Nmae: {user.name}</h2>
+          <h4>Email: {user.email}</h4>
+          <img src={user.photoURL} alt="" />
+        </div>
+      }
+
     </div>
   );
 }
